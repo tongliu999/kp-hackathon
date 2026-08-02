050c6f4fb7ed9dacb6b40a14ca1f4b5c2f7d1565
Shyamsaibethina <shyamsaibethina@gmail.com>
2026-08-01 19:19:25 -0700

    TON-14 add asynchronous cold-task callbacks

--- src/runbook_voice/__init__.py
+++ src/runbook_voice/__init__.py
@@ -1,8 +1,17 @@
 """Runbook Voice Assistant core package."""
 
 from .audio import AudioClip
+from .cold_tasks import ColdTaskCoordinator, JobSnapshot, JobStatus
 from .voice import LatencyReport, VoiceEchoLoop
 
 __version__ = "0.1.0"
 
-__all__ = ["AudioClip", "LatencyReport", "VoiceEchoLoop", "__version__"]
+__all__ = [
+    "AudioClip",
+    "ColdTaskCoordinator",
+    "JobSnapshot",
+    "JobStatus",
+    "LatencyReport",
+    "VoiceEchoLoop",
+    "__version__",
+]
